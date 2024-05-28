a1e788ea66dc64e7c8f9d4c4d82fc2b1da3a2743
Joep van Dormolen <j.van.dormolen@protonmail.com>
2024-05-28 22:09:58 +0200

    added more filter options. Finished refactoring the select options.

--- client/src/components/form.js
+++ client/src/components/form.js
@@ -1,14 +1,96 @@
 import {Formselect} from "./formselect";
+import {useState} from "react";
 
 export function Form() {
+  const [formInput, setFormInput] = useState("");
+
+  const filterOptions = [
+    [
+      {
+        mealtypes: ["Breakfast", "Lunch", "Dinner", "Snack"],
+        placeholder: "Meal",
+        id: "meal_menu",
+        txt: "Pick a meal",
+      },
+    ],
+    [
+      {
+        cuisineType: [
+          "American",
+          "Asian",
+          "British",
+          "Italian",
+          "Mediterranean",
+          "Chinese",
+          "Caribbean",
+        ],
+        placeholder: "Cuisine",
+        id: "cuisine_menu",
+        txt: "Pick a cuisine",
+      },
+    ],
+    [
+      {
+        health: [
+          "celery-free",
+          "gluten-free",
+          "vegetarian",
+          "vegan",
+          "soy-free",
+          "kosher",
+          "wheat-free",
+          "fish-free",
+          "egg-free",
+          "shellfish-free",
+        ],
+        placeholder: "health",
+        id: "health_menu",
+        txt: "Diet preferences",
+      },
+    ],
+    [
+      {
+        dishType: [
+          "Bread",
+          "Cereals",
+          "Desserts",
+          "Drinks",
+          "Main course",
+          "Pancake",
+          "Salad",
+          "Preserve",
+          "Sandwiches",
+          "Side dish",
+          "Soup",
+          "Starter",
+          "Sweets",
+        ],
+        placeholder: "Dish",
+        id: "dish_menu",
+        txt: "Dish options",
+      },
+    ],
+  ];
+
+  const handleInputChange = (e) => {
+    setFormInput(e.target.value);
+  };
+
   return (
-    <form className="container mx-auto py-8">
+    <form className="container  py-8">
       <input
-        type="search"
+        type="input"
+        name="query"
         placeholder="Enter your ingredients..."
-        className="w-full h-16 px-3 text-black rounded mb-8 focus:outline-none focus:shadow-lg text-xl shadow-lg"
+        className="w-full h-16 px-3 text-black rounded  focus:outline-none focus:shadow-lg text-xl shadow-lg"
+        value={formInput}
+        onChange={handleInputChange}
       ></input>
-      <Formselect />
+      <div className="grid grid-cols-4 ">
+        {filterOptions.map((item) => (
+          <Formselect key={item[0].id} menuOptions={item} />
+        ))}
+      </div>
     </form>
   );
 }
--- client/src/components/formselect.js
+++ client/src/components/formselect.js
@@ -1,22 +1,38 @@
 import {useState} from "react";
-export function Formselect() {
-  const [mealMenu, setMealMenu] = useState(true);
+import {useRef} from "react";
+export function Formselect({menuOptions}) {
+  const [options, setOptions] = useState(false);
+  const catMenu = useRef(null);
 
-  const mealTypes = [
-    {
-      mealtypes: ["Breakfast", "Lunch", "Dinner", "Snack"],
-      placeholder: "Meal",
-      id: "meal_menu",
-      txt: "Pick a meal",
-    },
-  ];
+  const [selectedOptions, setSelectedOptions] = useState([]);
+
   const handleClick = () => {
-    setMealMenu(!mealMenu);
+    setOptions(!options);
+  };
+  const menuOpen = (e) => {
+    if (options && !catMenu.current?.contains(e.target)) {
+      handleClick();
+    }
+  };
+  document.addEventListener("mousedown", menuOpen);
+
+  const toggleBackground = (e) => {
+    e.target.classList.contains("bg-orange-400")
+      ? e.target.classList.remove("bg-orange-400")
+      : e.target.classList.add("bg-orange-400");
   };
 
-  const items = mealTypes.map((opt) => {
+  const handleOptions = (e) => {
+    let options = [...selectedOptions];
+    e.target.classList.contains("bg-orange-400")
+      ? options.push(e.target.textContent)
+      : (options = options.filter((item) => item !== e.target.textContent));
+    setSelectedOptions(options);
+  };
+
+  const items = menuOptions.map((opt) => {
     return (
-      <div className="flex relative py-4 px-2 text-left" key={opt.id}>
+      <div className=" relative py-2    text-left" key={opt.id}>
         <div>
           <button
             type="button"
@@ -35,110 +51,49 @@ export function Formselect() {
         </div>
         <div
           role="menu"
+          ref={catMenu}
           aria-orientation="vertical"
           aria-labelledby={opt.id}
           tabIndex="-1"
           className={
-            "absolute top-10  left-0 z-10 mt-2 origin-top-right  rounded-lg bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none " +
-            (mealMenu ? " transition ease-out " : " transition ease-in ") +
-            (mealMenu ? " duration-100 " : " duration-75 ") +
-            (mealMenu ? " transform opacity-100 " : " transform opacity-0 ") +
-            (mealMenu ? " scale-100 " : " scale-95 ")
+            "absolute top-10   left-0 z-10 mt-2 origin-top-right  rounded-lg bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none " +
+            (options ? " transition ease-out " : " transition ease-in ") +
+            (options ? " duration-100 " : " duration-75 ") +
+            (options ? " transform opacity-100 " : " transform opacity-0 ") +
+            (options ? " scale-100 " : " scale-95 ")
           }
         >
-          <select
+          <ul
             id={opt.placeholder}
             placeholder={opt.placeholder}
             name={Object.keys(opt)[0]}
             multiple
             className="bg-orange-600 border p-0 font-sans  border-gray-300 text-gray-900 text-sm rounded-lg  block w-full dark:bg-orange-600 dark:border-gray-600  dark:text-gray-900 font-semibold "
           >
-            <option className="hover:bg-orange-400  hover:rounded focus:bg-orange-400">
+            <li className="hover:bg-orange-400 rounded  hover:rounded focus:bg-orange-400 px-1">
               {opt.txt}
-            </option>
+            </li>
 
             {opt[Object.keys(opt)[0]].map((elem) => {
               return (
-                <option
+                <li
                   key={elem}
-                  className="hover:bg-orange-400  hover:rounded focus:bg-orange-400"
+                  className="hover:bg-orange-400 rounded hover:rounded focus:bg-orange-400 px-1"
                   value={elem}
+                  onClick={(e) => {
+                    toggleBackground(e);
+                    handleOptions(e);
+                  }}
                 >
                   {elem}
-                </option>
+                </li>
               );
             })}
-          </select>
+          </ul>
         </div>
       </div>
     );
   });
 
   return <>{items}</>;
-  // <div className="flex relative py-4 px-2 text-left">
-  //   <div>
-  //     <button
-  //       type="button"
-  //       onClick={handleClick}
-  //       className="block justify-center bg-orange-600 rounded px-2 py-1
-  //     hover:bg-orange-800 hover:ring-gray-900 ring-1 hover:ring-2 focus:ring-2 focus:ring-gray-900
-
-  //      text-sm text-gray-800 font-semibold mb-2"
-  //       aria-expanded="true"
-  //       aria-haspopup="true"
-  //       id="meal_menu"
-  //     >
-  //       Meal
-  //     </button>
-  //   </div>
-  //   <div
-  //     role="menu"
-  //     aria-orientation="vertical"
-  //     aria-labelledby="meal_menu"
-  //     tabIndex="-1"
-  //     className={
-  //       "absolute top-10  left-0 z-10 mt-2 origin-top-right  rounded-lg bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none " +
-  //       (mealMenu ? " transition ease-out " : " transition ease-in ") +
-  //       (mealMenu ? " duration-100 " : " duration-75 ") +
-  //       (mealMenu ? " transform opacity-100 " : " transform opacity-0 ") +
-  //       (mealMenu ? " scale-100 " : " scale-95 ")
-  //     }
-  //   >
-  //     <select
-  //       id="meals"
-  //       placeholder="Meal"
-  //       name="mealtypes"
-  //       multiple
-  //       className="bg-orange-600 border p-0 font-sans  border-gray-300 text-gray-900 text-sm rounded-lg  block w-full dark:bg-orange-600 dark:border-gray-600  dark:text-gray-900 font-semibold "
-  //     >
-  //       <option className="hover:bg-orange-400  hover:rounded focus:bg-orange-400">
-  //         Pick a meal
-  //       </option>
-  //       <option
-  //         className="hover:bg-orange-400  hover:rounded focus:bg-orange-400"
-  //         value="Breakfast"
-  //       >
-  //         Breakfast
-  //       </option>
-  //       <option
-  //         className="hover:bg-orange-400 hover:rounded focus:bg-orange-400"
-  //         value="Lunch"
-  //       >
-  //         Lunch
-  //       </option>
-  //       <option
-  //         className="hover:bg-orange-400 hover:rounded focus:bg-orange-400"
-  //         value="Dinner"
-  //       >
-  //         Dinner
-  //       </option>
-  //       <option
-  //         className="hover:bg-orange-400 hover:rounded focus:bg-orange-400"
-  //         value="Snack"
-  //       >
-  //         Snack
-  //       </option>
-  //     </select>
-  //   </div>
-  // </div>
 }
