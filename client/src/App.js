0ea0dba4141261f58255876ae3cdbbf7311017e8
Joep van Dormolen <j.van.dormolen@protonmail.com>
2024-05-30 17:28:18 +0200

    Created a more responsive and much better looking form. Also refactored the components in it so they can be reused in other areas of the app. Like the submit button and the input elements.

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,14 @@
 import {Navbar} from "./navbar";
-import {Form} from "./components/form";
+
+import {Form} from "./components/recipeform";
+
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Navbar />
-        <section className="bg-indigo-500 h-50 p-8 rounded">
-          <Form />
-        </section>
-      </header>
+      <Navbar />
+      <Form />
     </div>
   );
 }
