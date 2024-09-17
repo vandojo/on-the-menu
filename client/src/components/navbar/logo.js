36a4eac4816152ffbd3a5d31473f16bb92f2ba51
Joep van Dormolen <j.van.dormolen@protonmail.com>
2024-09-17 15:18:04 +0200

    Fixed the link in the navbar, to open in a new tab and point to my profile page instead of the project repo

--- client/src/components/navbar/logo.js
+++ client/src/components/navbar/logo.js
@@ -1,7 +1,9 @@
 export function Logo() {
   return (
     <a
-      href="https://github.com/vandojo/on-the-menu"
+      href="https://github.com/vandojo"
+      target="_blank"
+      rel="noreferrer noopener"
       className="flex items-center py-4 px-2 hover:text-fuchsia-400 hover:underline text-white"
     >
       VanDoJo
